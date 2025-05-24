2e7085e95282e5f08553f02ab915e520ec079a21
Amancio <amancio.jluis@outlook.com>
2025-05-24 09:44:32 -0300

    JLA - Correção do Teste do botão voltar

--- cypress/e2e/telaInicial.cy.js
+++ cypress/e2e/telaInicial.cy.js
@@ -15,9 +15,20 @@ describe('Testes na tela index do projeto', () => {
         .should('have.text', ' Automação de testes').as('Sub-Título')
     })
 
-
-    it('Testar botão voltar', () =>{
-        cy.get('[href="./login.html"] > button')
-            .click()
+    it.only('Testar botão voltar', () =>{
+        cy.visit('/texto.html');
+        cy.get('a[href="./index.html"]').should('be.visible', 'Voltar Ao ìnicio').click()
     })
+
+      it.only('Validar retorno após clicar no botão "Voltar', () =>{
+        cy.contains('h1', 'Curso Automação de Testes Bàsico').should('be.visible')
+    })
+
+/*
+    Esta solução resolveu a questão do teste mas o deixou não elegante com o uso de cy.visit() específio e a necessidade de criar mais um teste para validar se retornou para a página correta.
+    Neste caso, o correto seria alinhar com o time DEV para efetuar a correção. 
+
+    **** PRECISO APROFUNDAR ESSE TEMA *** 
+*/
+
 })
